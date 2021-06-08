653702fe01b887b83b19dd6ac5c402c52bc309b8
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2021-06-08 13:27:21 -0400

    can generate and read data

--- takehome3/q2.py
+++ takehome3/q2.py
@@ -25,26 +25,45 @@ def dataFromGMM(N, priors, means, covs):
 
     return (x, labels)
 
-def writeData(C, N, fname):
+def writeSamples(C, N, E):
     """Generate the real dataset for X and write to file"""
-    # C is the number of distinct covarience matrices
-    # create means by evenly spacing on a line
-    means = np.array([np.linspace(-(C),C,C), np.zeros(C)]).T
-    # evenly distribute them
-    priors = np.ones(C)/C
-    # C cov arrays that are all 
-    covs = np.zeros((C, d, d))
-    for i in range(C):
-        covs[i,:,:] = np.eye(d) * random.uniform(0.1, 0.5) + (np.ones((d,d)) - np.eye(d)) * random.uniform(-1, 1)
+    x = np.zeros((E, d, N))
+    labels = np.zeros((E, N))
+    for e in range(E):
+        # C is the number of distinct covarience matrices
+        # create means by evenly spacing on a line
+        means = np.array([np.linspace(-(C),C,C), np.zeros(C)]).T
+        # evenly distribute them
+        priors = np.ones(C)/C
+        # C cov arrays that are all 
+        covs = np.zeros((C, d, d))
+        for i in range(C):
+            covs[i,:,:] = np.eye(d) * random.uniform(0.1, 0.5) + (np.ones((d,d)) - np.eye(d)) * random.uniform(-1, 1)
 
+        x[e, :, :], labels[e, :] = dataFromGMM(N, priors, means, covs)
 
-    x, labels = dataFromGMM(N, priors, means, covs)
+    np.save(f"data/{N}samples_set", x)
+    np.save(f"data/{N}samples_labels", labels)
 
-    plt.scatter(x[0], x[1], c=labels)
-    plt.show()
-
-def readData():
+def readSamples(N):
     """Read the generated datasets from file"""
-    pass
+    x = np.load(f"data/{N}samples_set.npy")
+    labels = np.load(f"data/{N}samples_labels.npy")
+    return x, labels
 
-writeData(6, 500, "")
+def createData(E, P):
+    """Creates all datasets and writes them to the data folder
+        E -- number of experiments
+        P -- range of powers to apply to 10 for the number of samples
+    """
+    for p in P:
+        writeSamples(6, 10**p, E)
+
+def readAll(P):
+    ret = []
+    for p in P:
+        ret.append(readSamples(10**p))
+    return ret
+
+#createData(100, range(2,7))
+allData = readAll(range(2,7))
